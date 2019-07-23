50971d868c448741a80871af69c4ce1ade525eb8
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-07-23 15:25:11 +0200

    added settings handling

---
+++ R/default_settings.R
@@ -0,0 +1,49 @@
+# The code in this file manages the settings for the different methods
+
+default_settings <- list(
+  gq = list(
+    gq.quad_points = 5
+  )
+  # agq = list(
+  #   derivative_step = 1E-6,
+  #   adaptive = TRUE,
+  #   adaptation_mode = 'mode',
+  #   scaled_likelihood = TRUE,
+  #   gq.quad_points = 5,
+  #   y_integration.n_samples = 1000,
+  #   y_integration.method = 'mc',
+  #   b_integration.method = 'gq',
+  #   jit = FALSE,
+  #   seed = 12345,
+  #   record_likelihood_evaluations = F
+  # ),
+  # laplace = list(
+  #   derivative_step = 1E-6,
+  #   adaptive = TRUE,
+  #   adaptation_mode = 'mode',
+  #   scaled_likelihood = TRUE,
+  #   gq.quad_points = 1,
+  #   y_integration.n_samples = 1000,
+  #   y_integration.method = 'mc',
+  #   b_integration.method = 'gq',
+  #   jit = FALSE,
+  #   seed = 12345,
+  #   record_likelihood_evaluations = F
+  # )
+)
+
+for(group_name in names(default_settings)){
+  settings <- default_settings[[group_name]]
+  f <- function(){
+    for(name in names(as.list(formals()))) settings[[name]] <- get(name)
+    return(settings)
+  }
+  formals(f) <- as.pairlist(default_settings[[group_name]])
+  assign(paste0("defaults.", group_name), f)
+}
+
+filter_settings <- function(settings, prefix){
+  filtered <- settings[grepl(paste0(prefix,"."), names(settings))]
+  names(filtered) <- gsub(paste0(prefix,"."),"", names(filtered))
+  filtered
+}
