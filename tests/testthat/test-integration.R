20c0b349d686179463451bbb4d73bd7c82ebd408
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-07-24 12:41:02 +0200

    added multidim test

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -1,4 +1,9 @@
-test_that("expected value calculation using gq works", {
+test_that("expected value calculation using gq works in 1D", {
   f <- function(x) x^2
   expect_equal(expect_norm_gq(f), 1)
+})
+
+test_that("expected value calculation using gq works in 4D", {
+  f <- function(x) x^2
+  expect_equal(expect_norm_gq(f, 4), c(1,1,1,1))
 })
