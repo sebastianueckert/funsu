b70ff6151afa156a86e6aa77bf6e2c02081f4d62
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-07-25 17:39:12 +0200

    added test for non-centered normal

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -1,9 +1,18 @@
-test_that("expected value calculation using gq works in 1D", {
+test_that("gq expected value calculation in 1D works", {
   f <- function(x) x^2
+  # chi-square distribution
   expect_equal(expect_norm_gq(f), 1)
+
+  #non-central chi-square
+  expect_equal(expect_norm_gq(f, center = 1), 2)
+
 })
 
-test_that("expected value calculation using gq works in 4D", {
+test_that("gq expected value calculation in 4D works", {
   f <- function(x) x^2
   expect_equal(expect_norm_gq(f, 4), c(1,1,1,1))
+
+  #non-central chi-square
+  expect_equal(expect_norm_gq(f, 4, center = 1), c(2,2,2,2))
+
 })
