aa2f07aa3d8f37128b2b4a4d032e95c79df0f094
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-07-26 17:23:22 +0200

    added expected cov matrix calculation

--- R/irt.R
+++ R/irt.R
@@ -11,7 +11,8 @@
 #' @export
 #'
 #' @examples
-#' fit <- mirt::mirt()
+#' fit <- mirt::mirt(mirt::Science, 1)
+#' irt_expected_item_response(fit)
 irt_expected_item_response <- function(model, mu = NULL, sigma = NULL){
   if(!requireNamespace("mirt", quietly = TRUE)) stop("The mirt package is need for this function.", call. = F)
   if(is.null(mu))  mu <- mirt::coef(model, simplify = T)$means
@@ -25,4 +26,57 @@ irt_expected_item_response <- function(model, mu = NULL, sigma = NULL){
   p_i <-  purrr::map(item_objs, ~expect_norm_gq(mirt::probtrace, x = .x, dimensions = dim, mu = mu, sigma = sigma))
   expect_i <- purrr::map_dbl(p_i, ~sum(.x * (seq_along(.x)-1))) %>% purrr::set_names(item_names)
   return(expect_i)
+}
+
+
+#' Calculate expected item covariance matrix for a population
+#'
+#' @param model A \code{mirt} model
+#' @param mu The mean of latent variable values
+#' @param sigma The variance-covariance matrix of the latent variable values
+#'
+#' @return The variance covariance matrix of the assessment
+#' @export
+#'
+#' @examples
+#' fit <- mirt::mirt(mirt::Science, 1)
+#' irt_expected_cov_matrix(fit)
+irt_expected_cov_matrix <- function(model, mu = NULL, sigma = NULL){
+  if(!requireNamespace("mirt", quietly = TRUE)) stop("The mirt package is need for this function.", call. = F)
+  if(is.null(mu))  mu <- mirt::coef(model, simplify = T)$means
+  if(is.null(sigma)) sigma <- mirt::coef(model, simplify = T)$cov
+  dim <- model@Model$nfact
+  stopifnot(dim == length(mu))
+  stopifnot(dim == NROW(sigma))
+
+  nitems <- model@Data$nitems
+  item_names <- colnames(model@Data$data)
+  item_objs <- purrr::map(seq_len(nitems), ~mirt::extract.item(model, .x))
+  p_i <-  purrr::map(item_objs, ~expect_norm_gq(mirt::probtrace, x = .x, dimensions = dim, mu = mu, sigma = sigma))
+  expect_i <- purrr::map_dbl(p_i, ~sum(.x * (seq_along(.x)-1)))
+  var_i <- purrr::imap_dbl(p_i, ~sum(.x * ((seq_along(.x)-1) - expect_i[.y])^2))
+  cov_matrix <- diag(var_i)
+  for(i in seq_len(nitems)){
+    for(j in seq_len(nitems)){
+      if(j>=i) next
+      prob_ij <- function(psi) {
+        p_i <- mirt::probtrace(item_objs[[i]], psi)
+        p_j <- mirt::probtrace(item_objs[[j]], psi)
+        p_ij <- matrix(nrow = NROW(psi), ncol = ncol(p_i)*ncol(p_j))
+        for(n in seq_len(NROW(psi))) p_ij[n,] <- as.vector(p_i[n,]%*%t(p_j[n,]))
+        return(p_ij)
+      }
+      p_ij <- expect_norm_gq(prob_ij, dimensions = dim, mu = mu, sigma = sigma) %>%
+        matrix(nrow = 4)
+      covar <- 0
+      for(k_i in seq_len(item_objs[[i]]@ncat)){
+        for(k_j in seq_len(item_objs[[j]]@ncat)){
+          covar <- covar + p_ij[k_i, k_j]*(k_i-1-expect_i[i])*(k_j-1-expect_i[j])
+        }
+      }
+      cov_matrix[i,j] <- cov_matrix[j,i] <- covar
+    }
+  }
+  colnames(cov_matrix) <- rownames(cov_matrix) <- item_names
+  cov_matrix
 }
--- tests/testthat/test-irt.R
+++ tests/testthat/test-irt.R
@@ -1,5 +1,43 @@
+fit <- mirt::mirt(mirt::Science, 1, verbose = F)
+
+
 test_that("expectation calculation works", {
-  expected <- c(Comfort = 2.12020087820744, Work = 1.72405134617488, Future = 1.9887845186995,  Benefit = 1.83671835360009)
-  fit <- mirt::mirt(mirt::Science, 1, verbose = F)
+  expected <-
+    c(
+      Comfort = 2.12020087820744,
+      Work = 1.72405134617488,
+      Future = 1.9887845186995,
+      Benefit = 1.83671835360009
+    )
   expect_equal(irt_expected_item_response(fit), expected)
+})
+
+test_that("covariance calculation works", {
+  expected <-
+    structure(
+      c(
+        0.349472186051655,
+        0.105930771347902,
+        0.140622699245503,
+        0.0986103365223237,
+        0.105930771347902,
+        0.65430318337363,
+        0.238568664281186,
+        0.166798309159225,
+        0.140622699245503,
+        0.238568664281186,
+        0.575280920775503,
+        0.221589654473857,
+        0.0986103365223237,
+        0.166798309159225,
+        0.221589654473857,
+        0.648215038247966
+      ),
+      .Dim = c(4L, 4L),
+      .Dimnames = list(
+        c("Comfort",  "Work", "Future", "Benefit"),
+        c("Comfort", "Work", "Future",  "Benefit")
+      )
+    )
+  expect_equal(irt_expected_cov_matrix(fit), expected)
 })
