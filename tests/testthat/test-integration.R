af72337be1d5316ceec98b20acaa2bd13d83f729
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-07-26 17:51:31 +0200

    added MC tests

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -16,3 +16,13 @@ test_that("gq expected value calculation in 4D works", {
   expect_equal(expect_norm_gq(f, 4, mu = c(1,0,2,3)), c(2,1,5,10))
 
 })
+
+test_that("mc expected value calculation in 4D works", {
+  f <- function(x) x^2
+  expect_lt(abs(mean(expect_norm_mc(f, 4)-c(1,1,1,1))), 0.1)
+
+  #non-central chi-square
+  expect_lt(abs(mean(expect_norm_mc(f, 4, mu = c(1,0,2,3))-c(2,1,5,10))), 0.1)
+
+})
+
